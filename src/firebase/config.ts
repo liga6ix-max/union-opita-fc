33ffbc6d8e6f795e66f8427ef63eaafed26af936
liga6ix torneo de barrio <liga6ix@gmail.com>
2026-01-12 21:50:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6589981828-37770",
+  "appId": "1:594268994262:web:2104edf314b148311e2777",
+  "apiKey": "AIzaSyB_NIXoIUclAl6llNYcVdA69hd3U0Yzf0E",
+  "authDomain": "studio-6589981828-37770.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "594268994262"
+};
